3a0073b0a8b214b6ecf2f58fa9850e6cbda526e9
prabhat sharma <prabhat.04011998@gmail.com>
2019-02-15 12:48:55 +0530

    updated

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+const exphbs=require('express-handlebars');
 
 app.use(express.static(path.join(__dirname,'public')));
+app.engine('handlebars',exphbs({defaultLayout: 'home'}))
+app.set('view engine','handlebars');
 app.get('/',(req,res)=>{
-    res.send('it works')
+    res.render('home/index')
 })
 app.listen(4500,()=>{
     console.log(`server is listing on the port 4500`);
