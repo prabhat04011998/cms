d9b854dce5ceb2fccb04af7d9859f843f00f2c85
prabhat sharma <prabhat.04011998@gmail.com>
2019-02-16 12:22:58 +0530

    admin panel start

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const path=require('path');
 const exphbs=require('express-handlebars');
 
 app.use(express.static(path.join(__dirname,'public')));
+//Set View Engine
 app.engine('handlebars',exphbs({defaultLayout: 'home'}))
 app.set('view engine','handlebars');
-app.get('/',(req,res)=>{
-    res.render('home/index')
-})
+//load routes
+const home=require('./routes/home');
+const admin=require('./routes/admin');
+//use routes
+app.use('/',home); 
+app.use('/admin',admin)
 app.listen(4500,()=>{
     console.log(`server is listing on the port 4500`);
 })
